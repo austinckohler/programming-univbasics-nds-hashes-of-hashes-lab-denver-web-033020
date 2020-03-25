45074131cffeeb826ccc7c20476c9d31048abd4e
Austin Kohler <austinckohler@gmail.com>
2020-03-25 21:28:44 +0000

    Automatically backed up by Learn

---
+++ notes.rb
@@ -0,0 +1,17 @@
+child = {
+  :name => "carrie",
+  :mother => {
+    :name: "debbi",
+    :mother: => {
+      :name => "Maxene"
+    },
+    :father => {}
+    },
+    :father =>{
+      :name => "Eddie",
+      :mother => {},
+      :father => {}
+    }
+}
+
+puts child[:name]
